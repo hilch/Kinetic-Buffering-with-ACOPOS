Calculate buffering current and correct scaling for capacity

src/KineticBufferingPowerCalc.py
@@ -10,22 +10,34 @@ class Axis:
     '''
     ACOPOS driven master axis used for kinetic buffering
     '''
-    def __init__(self, aptfile, loadInertia, nPowerFail, Pfriction, C ):
+    def __init__(self, aptfile, 
+                 iGear = 1.0,
+                 loadInertia = 0.0, 
+                 speedPowerfail = 10.0, 
+                 Pfriction = 0, 
+                 UDC_NOMINAL = 750,
+                 DCbusCapacity = 1650
+                   ):
         '''
 
         Args:
-            aptfile : ACOPOS parameter file with motor data *.apt
-            shaftInertia : additional shaft inertia reduced by gear [kgm^2]
-            nPowerFail : shaft speed in case of power failure [1/s]
-            Pfriction : fiction power which consumes the kinetic energy, e.g. UDC_ACT * ICTRL_ISQ_ACT [W]
-            C : DC bus capacity [F]
+            aptfile : ACOPOS parameter file with motor specification *.apt
+            iGear : gear ratio
+            loadInertia : load inertia [kgm^2]
+            speedPowerfail : gear shaft speed in case of powerfail [1/s]
+            Pfriction : fiction power which consumes the kinetic energy, e.g. MOTOR_TERMINAL_POWER  [W]
+            UDC_NOMINAL : nominal DC bus voltage before powerfail
+            DCbusCapacity : DC bus capacity [µF]
         '''
+
+        self.motorShaftInertia = loadInertia / iGear**2
+        self.nPowerFail = speedPowerfail * iGear # motor shaft speed in case of powerfail [1/s]
+        self.Pfriction = Pfriction
+        self.Ecap = 1/2 * (DCbusCapacity/1.0e6) * UDC_NOMINAL**2  # electrical energy before powerfail
+
+        # read ACOPOS parameter table with motor specification
         tree = ET.parse(aptfile)
         root = tree.getroot()
-        self.loadInertia = loadInertia
-        self.nPowerFail = nPowerFail
-        self.Pfriction = Pfriction
-
         if root.tag == 'AcoposParameterTable':
             parameters = root[0]
             if( parameters.tag == 'Root' and parameters.attrib == {'Name':'Parameters'}):
@@ -69,62 +81,66 @@ class Axis:
                                     self.MOTOR_INERTIA = value
 
             omega = 2 * np.pi * self.nPowerFail
-            self.Erot = 1/2 * (self.MOTOR_INERTIA + self.loadInertia) * omega**2 # [Ws]
-            self.Ecap = 1/2 * C * 620**2
+            self.Erot = 1/2 * (self.MOTOR_INERTIA + self.motorShaftInertia) * omega**2 # [Ws]
             pass
 
         else:
             raise TypeError(f"{aptfile} does not contain an ACOPOS parameter table !")
 
 
-    def _plotPregen(self, axs, Pregen, Pregen0, iq, n, n0):
+    def _plotPregen(self, ax, Pregen, Pregen0, iq, n, n0):
         '''
         plot regenerative power as function of current
         '''
         for i in range( len(n)):
-            axs[0, 0].plot( iq, Pregen[i], '--')
-        axs[0, 0].plot(iq, Pregen0, 'r', linewidth=2)
-        axs[0, 0].set_xlabel('quadrature current [A]')
-        axs[0, 0].set_ylabel('regenerative power [W]')
-        axs[0, 0].legend(['n = ' + str(round(n, 2)) for n in n] + ['n0 = ' + str(round(n0, 2))])
-        axs[0, 0].grid(True)
+            ax.plot( iq, Pregen[i], '--')
+        ax.plot(iq, Pregen0, 'r', linewidth=2)
+        ax.set_xlabel('quadrature current [A]')
+        ax.set_ylabel('regenerative power [W]')
+        ax.legend(['n = ' + str(round(n, 2)) for n in n] + ['n0 = ' + str(round(n0, 2))])
+        ax.grid(True)
 
 
-    def _plotPshaft(self, axs, Pshaft, iq, n, n0):
+    def _plotPshaft(self, ax, Pshaft, iq, n, n0):
         '''
         plot motor shaft power as function of current
         '''
         for i in range(len(n)):
-            axs[0, 1].plot(iq, Pshaft[i], '--')
-        axs[0, 1].set_xlabel('quadrature current [A]')
-        axs[0, 1].set_ylabel('shaft power [W]')
-        axs[0, 1].legend(['n = ' + str(round(n, 2)) for n in n] + ['n0 = ' + str(round(n0, 2))])
-        axs[0, 1].grid(True)
+            ax.plot(iq, Pshaft[i], '--')
+        ax.set_xlabel('quadrature current [A]')
+        ax.set_ylabel('shaft power [W]')
+        ax.legend(['n = ' + str(round(n, 2)) for n in n] + ['n0 = ' + str(round(n0, 2))])
+        ax.grid(True)
 
 
-    def _plotPloss(self, axs, Ploss, iq):
+    def _plotPloss(self, ax, Ploss, iq ):
         '''
         plot power loss as function of current
         '''
-        axs[0, 2].plot(iq, Ploss, 'r')
-        axs[0, 2].set_xlabel('quadrature current [A]')
-        axs[0, 2].set_ylabel('loss power [W]')
-        axs[0, 2].grid(True)        
+        pf = np.ones(101)*self.Pfriction
+        ax.plot(iq, Ploss, 'r')
+        ax.plot(iq, pf, 'b') 
+        ax.axvline(x=self.iqBuffer, color='r', linestyle='--') # mark buffer current               
+        ax.set_xlabel('quadrature current [A]')
+        ax.set_ylabel('loss power [W]')
+        ax.legend(['Ploss Cu', 'Ploss friction'])
+        ax.grid(True)        
 
 
-    def _plotBufferDuration(self, axs, Ploss, Pfriction, iq, iqmax ):
+    def _plotBufferDuration(self, ax, Ploss, iq ):
         '''
         buffer duration as function of (normalized) current
         '''
-        iqNormalized = np.divide( iq, iqmax)
+        #iqNormalized = np.divide( iq, iqmax)
         PlossTotal = Ploss + self.Pfriction # total power loss
         t = np.divide(self.Erot + self.Ecap, PlossTotal, where=PlossTotal!=0) # buffering time
         tBuffer = np.where(t <= 60, t, 60) # Replace values above the maximum
 
-        axs[1, 0].plot(iqNormalized, tBuffer, 'g')
-        axs[1, 0].set_xlabel('quadrature current (norm.)')
-        axs[1, 0].set_ylabel('time [s]')
-        axs[1, 0].grid(True)      
+        ax.plot(iq, tBuffer, 'g')
+        ax.axvline(x=self.iqBuffer, color='r', linestyle='--') # mark buffer current
+        ax.set_xlabel('quadrature current [A]')
+        ax.set_ylabel('time [s]')
+        ax.grid(True)      
 
 
     def _plotPowerSynchronous(self, axs):
@@ -139,15 +155,17 @@ class Axis:
         n = np.linspace(0, nN, 11)
         iq = np.linspace(0, iqmax, 101)
 
+        self.iqBuffer = self.Pfriction/(2*np.pi*self.nPowerFail*Kt)*np.sqrt(2)
+
         Ploss = 3/2* iq**2 * Rspp/2 # loss due to stator resistance
         Pshaft = Kt / np.sqrt(2) * 2 * np.pi * np.outer( n, iq)
         Pregen = Pshaft - Ploss
         Pregen0 = Kt / np.sqrt(2) * 2 * np.pi * n0 * iq - Ploss
 
-        self._plotPregen( axs, Pregen, Pregen0, iq, n, n0 )
-        self._plotPshaft( axs, Pshaft, iq, n, n0 )
-        self._plotPloss( axs, Ploss, iq)
-        self._plotBufferDuration( axs, Ploss, self.Pfriction, iq, iqmax)
+        self._plotPregen( axs[0,0], Pregen, Pregen0, iq, n, n0 )
+        self._plotPshaft( axs[0,1], Pshaft, iq, n, n0 )
+        self._plotPloss( axs[0,2], Ploss, iq )
+        self._plotBufferDuration( axs[1,0], Ploss, iq )
 
 
 
@@ -170,16 +188,17 @@ class Axis:
         iqmax = self.MOTOR_CURR_MAX * np.sqrt(2)        
         iq = np.linspace(0, iqmax, 101)
 
+        self.iqBuffer = self.Pfriction/(2*np.pi*self.nPowerFail*kt)*np.sqrt(2)
+
         Ploss = 3/2 * iq**2 * rs + 3/2 * i0**2 * rs # power loss due to stator resistance
         Pshaft = kt / np.sqrt(2) * 2 * np.pi * np.outer(n, iq)
         Pregen0 = kt / np.sqrt(2) * 2 * np.pi * n0 - Ploss
         Pregen = Pshaft - Ploss
 
-        self._plotPregen( axs, Pregen, Pregen0, iq, n, n0 )
-        self._plotPshaft( axs, Pshaft, iq, n, n0 )
-        self._plotPloss( axs, Ploss, iq)
-        self._plotBufferDuration( axs, Ploss, self.Pfriction, iq, iqmax)       
-
+        self._plotPregen( axs[0,0], Pregen, Pregen0, iq, n, n0 )
+        self._plotPshaft( axs[0,1], Pshaft, iq, n, n0 )
+        self._plotPloss( axs[0,2], Ploss, iq )
+        self._plotBufferDuration( axs[1,0], Ploss, iq)
 
     def plotPower(self):
         fig, axs = plt.subplots(2, 3)
@@ -195,20 +214,18 @@ class Axis:
         elif self.motorType == 'SM':
             self._plotPowerSynchronous(axs)
 
-        # add preconditions
+        # print some values 
         axs[1,2].axis('off')
         preconditions = (
-            f'n Fail = { round(self.nPowerFail,2)} 1/s',
+            f'motor shaft speed @ powerfail = { round(self.nPowerFail,2)} 1/s',
             f'P Friction = { round(self.Pfriction)} W',
             f'E rotation = { round(self.Erot/1000)} kJ',
-            f'E electr. = { round(self.Ecap/1000)} kJ'                        
+            f'E DC bus capacity = { round(self.Ecap/1000)} kJ',
+            f'iq KIB = { round(self.iqBuffer,1)} A'                                        
         )
         for n,text in enumerate(preconditions):
           axs[1,2].text( 0, 1-n*0.05, text, fontsize=12 )      
-        # axs[1,2].text( 0.1, 0., f'n Fail = { round(self.nPowerFail,2)} 1/s', fontsize=12 )    
-        # axs[1,2].text( 0.1, 0.2, f'P Friction = { round(self.Pfriction)} W', fontsize=12 )          
-        # axs[1,2].text( 0.1, 0.3, f'E rotation = { round(self.Erot/1000)} kJ', fontsize=12 )          
-        # axs[1,2].text( 0.1, 0.4, f'E electr. = { round(self.Ecap/1000)} kJ', fontsize=12 )          
+
 
         # save plot
         s = 'KIB-' + self.motorName
@@ -227,11 +244,18 @@ class Axis:
         
 
 if __name__ == '__main__':
-    # example data
-    motor = Axis("530d12f.apt", loadInertia= 4270, nPowerFail=1.5, Pfriction=22400, C= 2.64)
+    # example torque motor
+    motor = Axis("530d12f.apt", iGear=1, loadInertia= 4270, speedPowerfail=0.5, 
+                                Pfriction=7000, UDC_NOMINAL= 620, 
+                                DCbusCapacity= 1650 + 0.22 + 990 + 8 * 990 # 8BVP0880 + 8B0C0320 + 8BVI0440 + 8*8BVI0330
+                                )
     motor.plotPower() 
-    i=36   
-    motor = Axis("2kj3507p.apt", loadInertia=4270/i**2, nPowerFail=1.5072*i, Pfriction=22400, C= 2.64)
+ 
+    # example induction motor with gear
+    motor = Axis("2kj3507p.apt", iGear = 25, loadInertia=4270, speedPowerfail=0.5, 
+                                Pfriction=7000, UDC_NOMINAL= 620, 
+                                DCbusCapacity= 1650 + 0.22 + 990 + 8 * 990 # 8BVP0880 + 8B0C0320 + 8BVI0440 + 8*8BVI0330
+                                )
     motor.plotPower()
 
 